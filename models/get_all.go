68a3d6cbc64c136c978721b53dd0d55fa19eb49a
alessandra1408 <alessandraoitnb@gmail.com>
2023-09-07 01:49:29 -0300

    Create methods of CRUD

---
+++ models/get_all.go
@@ -0,0 +1,29 @@
+package models
+
+import "github.com/alessandra1408/crud-golang/db"
+
+func GetAll() (todos []Todo, err error) {
+	conn, cErr := db.NewPostgresConnection()
+	if cErr != nil {
+		return
+	}
+	defer conn.Close()
+
+	rows, qErr := conn.Query("SELECT * FROM todos")
+	if qErr != nil {
+		return
+	}
+
+	for rows.Next() {
+		var todo Todo
+		err = rows.Scan(&todo.ID, &todo.Title, todo.Description, &todo.Done)
+
+		if err != nil {
+			continue
+		}
+
+		todos = append(todos, todo)
+	}
+
+	return
+}
---
+++ models/insert.go
@@ -0,0 +1,18 @@
+package models
+
+import "github.com/alessandra1408/crud-golang/db"
+
+func Insert(todo Todo) (id int64, err error) {
+
+	conn, err := db.NewPostgresConnection()
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+
+	sql := "INSERT INTO todos(title, description, done) VALUES ($1, $2, $3, $4) RETURNING id"
+
+	err = conn.QueryRow(sql, todo.Title, todo.Description, todo.Done).Scan(&id)
+
+	return
+}
