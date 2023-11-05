894aac5f3fb471bdafbefeb3a47548d5301250c4
Alessandra Fernandes <alessandraoitnb@gmail.com>
2023-11-05 17:51:47 -0300

    create handlers

---
+++ handlers/create.go
@@ -0,0 +1,40 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/alessandra1408/crud-golang/models"
+)
+
+func Create(w http.ResponseWriter, r *http.Request) {
+	var todo models.Todo
+
+	err := json.NewDecoder(r.Body).Decode(&todo)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		log.Printf("Error while decode json: %v\n", err)
+		return
+	}
+
+	id, err := models.Insert(todo)
+
+	var resp map[string]any
+
+	if err != nil {
+		resp = map[string]any{
+			"Error":   true,
+			"Message": fmt.Sprintf("Some error occurred to insert:  %v", err),
+		}
+	} else {
+		resp = map[string]any{
+			"Error":   false,
+			"Message": fmt.Sprintf("Success to insert todo! ID:  %v", id),
+		}
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(resp)
+}
---
+++ handlers/get.go
@@ -0,0 +1,30 @@
+package handlers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/alessandra1408/crud-golang/models"
+	"github.com/go-chi/chi"
+)
+
+func Get(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		log.Printf("Error while decode json: %v\n", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	todo, err := models.Get(int64(id))
+	if err != nil {
+		log.Printf("Error while get register: %v\n", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(todo)
+}
