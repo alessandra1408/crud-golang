2ceb8dee6096db550fbb5a7355c37f76b2c75b66
alessandra1408 <alessandraoitnb@gmail.com>
2023-11-07 14:44:40 -0300

    create config file and main code

--- README.md
+++ README.md
@@ -5,5 +5,15 @@
 - Golang (GO)
 - Postgresql
 
+You will need a config.toml file to configure the local variables for this application like this:
+``[api]
+port = "port_numer"
+
+[database]
+host = ""
+user = ""
+pass = ""
+name = "" ``
+
 
 ###### Project made by Alessandra Fernandes :)
--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/alessandra1408/crud-golang/configs"
+	"github.com/alessandra1408/crud-golang/handlers"
+	"github.com/go-chi/chi/v5"
+)
+
 func main() {
-	
+
+	err := configs.Load()
+	if err != nil {
+		panic(fmt.Sprint("Error to load variables: %v", err))
+	}
+
+	r := chi.NewRouter()
+	r.Post("/", handlers.Create)
+	r.Put("/{id}", handlers.Update)
+	r.Delete("/{id}", handlers.Delete)
+	r.Get("/", handlers.Get)
+	r.Get("/{id}", handlers.List)
 }
